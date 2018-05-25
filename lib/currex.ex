5a91a4ecb0d91ca45fbabf1e5d57ac29a6311f11
ontofractal <ontofractal@protonmail.com>
2018-05-25 16:55:03 +0100

    add correct parsing of timestamps to NaiveDateTime

--- lib/currex.ex
+++ lib/currex.ex
@@ -21,11 +21,19 @@ defmodule Currex do
   def latest!(currency) do
     {:ok, tesla_env} = get("/#{currency}/")
     body = tesla_env.body
+    rates = body["rates"]
+    timestamp = body["timestamp"]
 
     %{
-      timestamp: DateTime.from_unix!(body["timestamp"]) |> DateTime.to_naive(),
-      rates: Enum.map(body["rates"], &AtomicMap.convert(&1, %{safe: false}))
+      timestamp: DateTime.from_unix!(timestamp) |> DateTime.to_naive(),
+      rates:
+        Enum.map(rates, fn datum when is_map(datum) ->
+          datum
+          |> AtomicMap.convert(%{safe: false})
+          |> Map.update!(:timestamp, fn timestamp ->
+            DateTime.from_unix!(timestamp) |> DateTime.to_naive()
+          end)
+        end)
     }
   end
-
 end
--- test/currex_test.exs
+++ test/currex_test.exs
@@ -4,7 +4,9 @@ defmodule CurrexTest do
 
   test "fetches rates in USD" do
     in_usd_latest = Currex.latest!("USD")
-    assert %{rates: _, timestamp: _} = in_usd_latest
-    assert %{currency: _, timestamp: _, rate: _} = hd(in_usd_latest.rates)
+    assert %{rates: _, timestamp: global_timestamp} = in_usd_latest
+    assert NaiveDateTime.to_iso8601(global_timestamp) |> NaiveDateTime.from_iso8601!() == global_timestamp
+    assert %{currency: _, timestamp: timestamp, rate: _} = hd(in_usd_latest.rates)
+    assert NaiveDateTime.to_iso8601(timestamp) |> NaiveDateTime.from_iso8601!() == timestamp
   end
 end
